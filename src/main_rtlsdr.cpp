639482fade7cf98731e5039aa3da1505daeea3b1
Attila Zseller <zsellera@gmail.com>
2026-02-03 12:30:30 +0100

    readme: hackrf vs rtl-sdr specifics

--- src/main_rtlsdr.cpp
+++ src/main_rtlsdr.cpp
@@ -87,7 +87,7 @@ int main(int argc, char** argv) {
             std::cerr << "Usage: " << argv[0] << " [-d ser_nr] [-g <gain_dB>] [-D] [-b] [-p tcp_port] [-m] [-t]\n";
             std::cerr << "\t-d ser_nr   default:first\tserial number of the desired RTL-SDR\n";
             std::cerr << "\t-g <dB>     default:" << DEFAULT_GAIN_TENTHS_DB / 10 << "  \ttuner gain in dB\n";
-            std::cerr << "\t-b          default:off \tEnable bias-tee (+4.5 V on RTL-SDR Blog V3/V4)\n";
+            std::cerr << "\t-b          default:off \tEnable bias-tee (+4.5 V)\n";
             std::cerr << "\t-p port     default:" << DEFAULT_ZEROMQ_PORT << "\tZeroMQ publisher port\n";
             std::cerr << "\t-m          default:off \tEnable monitor mode (print received frames to stdout)\n";
             std::cerr << "\t-t          default:off \tUse system clock as the timebase (beware of NTP jumps)\n";
