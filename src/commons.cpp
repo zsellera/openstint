0f37e491a20612dfb13bab6e77a65bdc123ccc97
Attila Zseller <zsellera@gmail.com>
2026-02-03 10:47:23 +0100

    refactor common logic from main.cpp to commons.cpp

---
+++ src/commons.cpp
@@ -0,0 +1,175 @@
+#include "commons.hpp"
+
+#include <cstdlib>
+#include <complex>
+#include <format>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <zmq.hpp>
+
+#include "preamble.hpp"
+#include "transponder.hpp"
+#include "frame.hpp"
+#include "passing.hpp"
+#include "counters.hpp"
+#include "timebase.hpp"
+
+
+static int zmq_port = DEFAULT_ZEROMQ_PORT;
+static zmq::context_t* zmq_context = nullptr;
+static zmq::socket_t* publisher = nullptr;
+
+static enum FrameParseMode { FRAME_SEEK, FRAME_FOUND } frame_parse_mode = FRAME_SEEK;
+static FrameDetector frame_detector(0.88f);
+static SymbolReader symbol_reader;
+static Frame frame;
+static PassingDetector passing_detector;
+static RxStatistics rx_stats;
+static bool monitor_mode = false;
+static Timebase timebase;
+
+
+bool process_frame(Frame* frame) {
+    const uint8_t *softbits = frame->bits();
+    if (!softbits) {
+        // preamble not found
+        return false;
+    }
+
+    if (monitor_mode) {
+        std::cout << "F " << *frame << std::endl;
+    }
+
+    uint32_t transponder_id;
+    switch (frame->transponder_type) {
+        case TransponderType::OpenStint:
+        if (decode_openstint(softbits, &transponder_id)) {
+            if (transponder_id < 10000000u) {
+                passing_detector.append(frame, transponder_id);
+            } else if ((transponder_id & 0x00A00000) == 0x00A00000) {
+                uint32_t transponder_timestamp = (transponder_id & 0x000FFFFF);
+                passing_detector.timesync(frame, transponder_timestamp);
+            }
+            return true;
+        }
+        break;
+        case TransponderType::Legacy:
+            if (decode_legacy(softbits, &transponder_id)) {
+                if (transponder_id < 10000000) { // extra check (7-digit max)
+                    passing_detector.append(frame, transponder_id);
+                }
+                return true;
+            }
+        break;
+    }
+    return false;
+}
+
+void detect_frames(const std::complex<int8_t>* samples, std::size_t sample_count) {
+    uint64_t buffer_timestamp = timebase.now();
+    bool frame_detected = false;
+    for (uint32_t idx=0; (idx+SAMPLES_PER_SYMBOL)<=sample_count; idx+=SAMPLES_PER_SYMBOL) {
+        if (frame_parse_mode == FRAME_SEEK) {
+            const std::optional<TransponderType> detected = frame_detector.process_baseband(samples+idx);
+            if (detected) {
+                frame_parse_mode = FRAME_FOUND;
+                frame_detected = true; // do not use this buffer for noisefloor calculation
+                uint64_t timestamp = buffer_timestamp + (1000 * idx) / SAMPLE_RATE;
+                frame = Frame(detected.value(), timestamp);
+                symbol_reader.read_preamble(&frame, frame_detector.dc_offset(), samples, idx+SAMPLES_PER_SYMBOL);
+            }
+        } else if (frame_parse_mode == FRAME_FOUND) {
+            symbol_reader.read_symbol(&frame, frame_detector.dc_offset(), samples+idx);
+            if (symbol_reader.is_frame_complete(&frame)) {
+                frame_parse_mode = FRAME_SEEK;
+                bool frame_processed = process_frame(&frame);
+                rx_stats.register_frame(frame_processed);
+            }
+        }
+    }
+
+    // save a small section of the buffer
+    // if there is a frame in the next buffer, and read_preamble() must
+    // look back, here save the trailing section of the current buffer
+    symbol_reader.update_reserve_buffer(samples, sample_count);
+
+    // update counters for noise energy and dc offset
+    if (frame_detected) {
+        // there was an actice frame in the buffer, do not update
+        // statistics, as the received data messes with the
+        // noise/dc-offset calculation
+        frame_detector.reset_statistics_counters();
+    } else {
+        frame_detector.update_statistics();
+        rx_stats.save_channel_characteristics(frame_detector.dc_offset(), frame_detector.noise_energy());
+    }
+}
+
+bool parse_common_arguments(int& i, const int argc, const std::string& arg, char** argv) {
+    if (arg == "-p" && i + 1 < argc) {
+        zmq_port = std::atoi(argv[++i]);
+    } else if (arg == "-m") {
+        monitor_mode = true;
+    } else if (arg == "-t") {
+        timebase.use_system_clock();
+    } else {
+        return false;
+    }
+    return true;
+}
+
+void init_commons() {
+    // transponder processing (allocate viterbi trellis); TODO RAII
+    init_transponders();
+
+    //  Prepare our context and publisher
+    std::string zmq_address;
+    std::format_to(std::back_inserter(zmq_address), "tcp://*:{}", zmq_port);
+    zmq_context = new zmq::context_t(1);
+    publisher = new zmq::socket_t(*zmq_context, zmq::socket_type::pub);
+    publisher->bind(zmq_address);
+    std::cout << "Listening on " << zmq_address << std::endl;
+}
+
+void report_detections() {
+    const auto now = timebase.now();
+
+    // report status once a second
+    if (rx_stats.reporting_due(now)) {
+        const std::string report = std::format("S {} {}", now, rx_stats.to_string());
+        rx_stats.reset(now);
+
+        std::cout << report << std::endl;
+        publisher->send(zmq::buffer(report), zmq::send_flags::none);
+    }
+    
+    std::vector<TimeSync> timesyncs = passing_detector.identify_timesyncs(500ul);
+    for (const auto& time_sync : timesyncs) {
+        const std::string report = std::format("T {} {} {} {}",
+            time_sync.timestamp,
+            transponder_props(time_sync.transponder_type).prefix, // always openstint
+            time_sync.transponder_id,
+            time_sync.transponder_timestamp
+        );
+
+        std::cout << report << std::endl;
+        publisher->send(zmq::buffer(report), zmq::send_flags::none);
+    }
+
+    std::vector<Passing> passings = passing_detector.identify_passings(now - 250ul);
+    for (const auto& passing : passings) {
+        const std::string report = std::format("P {} {} {} {:.2f} {} {}",
+            passing.timestamp,
+            transponder_props(passing.transponder_type).prefix,
+            passing.transponder_id,
+            passing.rssi,
+            passing.hits,
+            passing.duration
+        );
+
+        std::cout << report << std::endl;
+        publisher->send(zmq::buffer(report), zmq::send_flags::none);
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,7 @@
 #include <vector>
 
 #include "complex_cast.hpp"
+#include "commons.hpp"
 
 #include <zmq.hpp>
 #include <zmq_addon.hpp>
@@ -27,76 +28,19 @@
 #include <libhackrf/hackrf.h>
 #include <liquid/liquid.h>
 
-#include "preamble.hpp"
-#include "transponder.hpp"
-#include "frame.hpp"
-#include "passing.hpp"
-#include "counters.hpp"
-#include "timebase.hpp"
-
 
 static hackrf_device* device = nullptr;
 static std::atomic<bool> do_exit(false);
 
 static const uint64_t CENTER_FREQ_HZ       = 5000000ULL;
-static const uint32_t SYMBOL_RATE          = 1250000;
-static const uint32_t SAMPLE_RATE          = SYMBOL_RATE * SAMPLES_PER_SYMBOL;
 static const uint32_t BB_FILTER_BW         = 1750000;
 static const uint8_t DEFAULT_LNA_GAIN      = 24;           // 0-40 in steps of 8 or so; experiment
 static const uint8_t DEFAULT_VGA_GAIN      = 24;           // 0-62
-static const int DEFAULT_ZEROMQ_PORT       = 5556;
-
-static enum FrameParseMode { FRAME_SEEK, FRAME_FOUND } frame_parse_mode = FRAME_SEEK;
-static FrameDetector frame_detector(0.88f);
-static SymbolReader symbol_reader;
-static Frame frame;
-
-static PassingDetector passing_detector;
-static RxStatistics rx_stats;
-static bool monitor_mode = false;
-
-static Timebase timebase;
 
 // signal handler to break the capture loop
 void signal_handler(int signum) {
     std::cerr << "\nCaught signal " << signum << " — stopping...\n";
     do_exit = true;
-}
-
-bool process_frame(Frame* frame) {
-    const uint8_t *softbits = frame->bits();
-    if (!softbits) {
-        // preamble not found
-        return false;
-    }
-
-    if (monitor_mode) {
-        std::cout << "F " << *frame << std::endl;
-    }
-
-    uint32_t transponder_id;
-    switch (frame->transponder_type) {
-        case TransponderType::OpenStint:
-        if (decode_openstint(softbits, &transponder_id)) {
-            if (transponder_id < 10000000u) {
-                passing_detector.append(frame, transponder_id);
-            } else if ((transponder_id & 0x00A00000) == 0x00A00000) {
-                uint32_t transponder_timestamp = (transponder_id & 0x000FFFFF);
-                passing_detector.timesync(frame, transponder_timestamp);
-            }
-            return true;
-        }
-        break;
-        case TransponderType::Legacy:
-            if (decode_legacy(softbits, &transponder_id)) {
-                if (transponder_id < 10000000) { // extra check (7-digit max)
-                    passing_detector.append(frame, transponder_id);
-                }
-                return true;
-            }
-        break;
-    }
-    return false;
 }
 
 // hackrf callback invoked for each block of data
@@ -105,46 +49,10 @@ extern "C" int rx_callback(hackrf_transfer* transfer) {
         return 0;
     }
 
-    uint64_t buffer_timestamp = timebase.now();
     uint32_t sample_count = transfer->valid_length / 2;
     const std::complex<int8_t> *samples = reinterpret_cast<const std::complex<int8_t>*>(transfer->buffer);
     
-    bool frame_detected = false;
-    for (uint32_t idx=0; (idx+SAMPLES_PER_SYMBOL)<=sample_count; idx+=SAMPLES_PER_SYMBOL) {
-        if (frame_parse_mode == FRAME_SEEK) {
-            const std::optional<TransponderType> detected = frame_detector.process_baseband(samples+idx);
-            if (detected) {
-                frame_parse_mode = FRAME_FOUND;
-                frame_detected = true; // do not use this buffer for noisefloor calculation
-                uint64_t timestamp = buffer_timestamp + (1000 * idx) / SAMPLE_RATE;
-                frame = Frame(detected.value(), timestamp);
-                symbol_reader.read_preamble(&frame, frame_detector.dc_offset(), samples, idx+SAMPLES_PER_SYMBOL);
-            }
-        } else if (frame_parse_mode == FRAME_FOUND) {
-            symbol_reader.read_symbol(&frame, frame_detector.dc_offset(), samples+idx);
-            if (symbol_reader.is_frame_complete(&frame)) {
-                frame_parse_mode = FRAME_SEEK;
-                bool frame_processed = process_frame(&frame);
-                rx_stats.register_frame(frame_processed);
-            }
-        }
-    }
-
-    // save a small section of the buffer
-    // if there is a frame in the next buffer, and read_preamble() must
-    // look back, here save the trailing section of the current buffer
-    symbol_reader.update_reserve_buffer(samples, sample_count);
-
-    // update counters for noise energy and dc offset
-    if (frame_detected) {
-        // there was an actice frame in the buffer, do not update
-        // statistics, as the received data messes with the
-        // noise/dc-offset calculation
-        frame_detector.reset_statistics_counters();
-    } else {
-        frame_detector.update_statistics();
-        rx_stats.save_channel_characteristics(frame_detector.dc_offset(), frame_detector.noise_energy());
-    }
+    detect_frames(samples, sample_count);
 
     // Returning 0 indicates "keep going".
     return 0;
@@ -160,7 +68,6 @@ int main(int argc, char** argv) {
     uint8_t vga_gain = DEFAULT_VGA_GAIN;
     bool bias_tee = false;
     bool amp_enable = false; // hackrf has a custom, +13 dB preamp
-    int zmq_port = DEFAULT_ZEROMQ_PORT;
     const char* hackrf_serial = nullptr;
 
     // process command line arguments
@@ -187,23 +94,19 @@ int main(int argc, char** argv) {
             bias_tee = true;
         } else if (arg == "-a") {
             amp_enable = true;
-        } else if (arg == "-p" && i + 1 < argc) {
-            zmq_port = std::atoi(argv[++i]);
-        } else if (arg == "-m") {
-            monitor_mode = true;
-        } else if (arg == "-t") {
-            timebase.use_system_clock();
+        } else if (parse_common_arguments(i, argc, arg, argv)) {
+            // do nothing
         } else {
             if (arg != "-h") {
                 std::cerr << "Unknown argument: " << arg << "\n";
             }
-            std::cerr << "Usage: " << argv[0] << " [-d ser_nr] [-p tcp_port] [-l <0..40>] [-v <0..62>] [-a] [-b] [-m] [-t]\n";
+            std::cerr << "Usage: " << argv[0] << " [-d ser_nr] [-l <0..40>] [-v <0..62>] [-a] [-b] [-p tcp_port] [-m] [-t]\n";
             std::cerr << "\t-d ser_nr   default:first\tserial number of the desired HackRF\n";
-            std::cerr << "\t-p port     default:" << DEFAULT_ZEROMQ_PORT << "\tZeroMQ publisher port\n";
             std::cerr << "\t-l <0..40>  default:" << static_cast<int>(DEFAULT_LNA_GAIN) << "  \tLNA gain (rf signal amplifier; valid values: 0/8/16/24/32/40)\n";
             std::cerr << "\t-v <0..62>  default:" << static_cast<int>(DEFAULT_LNA_GAIN) << "  \tVGA gain (baseband signal amplifier, steps of 2)\n";
             std::cerr << "\t-a          default:off \tEnable preamp (+13 dB to input RF signal)\n";
             std::cerr << "\t-b          default:off \tEnable bias-tee (+3.3 V, 50 mA max)\n";
+            std::cerr << "\t-p port     default:" << DEFAULT_ZEROMQ_PORT << "\tZeroMQ publisher port\n";
             std::cerr << "\t-m          default:off \tEnable monitor mode (print received frames to stdout)\n";
             std::cerr << "\t-t          default:off \tUse system clock as the timebase (beware of NTP jumps)\n";
             
@@ -211,16 +114,7 @@ int main(int argc, char** argv) {
         }
     }
 
-    // transponder processing (allocate viterbi trellis); TODO RAII
-    init_transponders();
-
-    //  Prepare our context and publisher
-    std::string zmq_address;
-    std::format_to(std::back_inserter(zmq_address), "tcp://*:{}", zmq_port);
-    zmq::context_t context(1);
-    zmq::socket_t publisher(context, zmq::socket_type::pub);
-    publisher.bind(zmq_address);
-    std::cout << "Listening on " << zmq_address << std::endl;
+    init_commons();
 
     std::cout << "HackRF RX: freq=" << freq_hz << " Hz, sample_rate=" << sample_rate
               << " Hz, LNA=" << (int)lna_gain << ", VGA=" << (int)vga_gain << "\n";
@@ -310,44 +204,7 @@ int main(int argc, char** argv) {
     while (!do_exit && hackrf_is_streaming(device) == HACKRF_TRUE) {
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
         
-        const auto now = timebase.now();
-
-        // report status once a second
-        if (rx_stats.reporting_due(now)) {
-            const std::string report = std::format("S {} {}", now, rx_stats.to_string());
-            rx_stats.reset(now);
-
-            std::cout << report << std::endl;
-            publisher.send(zmq::buffer(report), zmq::send_flags::none);
-        }
-        
-        std::vector<TimeSync> timesyncs = passing_detector.identify_timesyncs(500ul);
-        for (const auto& time_sync : timesyncs) {
-            const std::string report = std::format("T {} {} {} {}",
-                time_sync.timestamp,
-                transponder_props(time_sync.transponder_type).prefix, // always openstint
-                time_sync.transponder_id,
-                time_sync.transponder_timestamp
-            );
-
-            std::cout << report << std::endl;
-            publisher.send(zmq::buffer(report), zmq::send_flags::none);
-        }
-
-        std::vector<Passing> passings = passing_detector.identify_passings(now - 250ul);
-        for (const auto& passing : passings) {
-            const std::string report = std::format("P {} {} {} {:.2f} {} {}",
-                passing.timestamp,
-                transponder_props(passing.transponder_type).prefix,
-                passing.transponder_id,
-                passing.rssi,
-                passing.hits,
-                passing.duration
-            );
-
-            std::cout << report << std::endl;
-            publisher.send(zmq::buffer(report), zmq::send_flags::none);
-        }
+        report_detections();
     }
 
     // stop RX
