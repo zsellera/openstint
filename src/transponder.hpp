87b169437d44d802be33a819acefadf85a6e29e6
oldfan <oldfan1@gmail.com>
2026-03-04 17:44:28 +0800

    Add RC4 support to TransponderType and properties

--- src/transponder.hpp
+++ src/transponder.hpp
@@ -6,7 +6,8 @@
 
 enum class TransponderType {
     OpenStint, // support for openstint protocol
-    Legacy     // legacy (Amb) protocol
+    Legacy,     // legacy (Amb) protocol
+    RC4         //RC4 (Amb RC4) protocol
 };
 
 struct TransponderProps {
@@ -18,10 +19,15 @@ struct TransponderProps {
 void init_transponders();
 int decode_openstint(const uint8_t *softbits, uint32_t *transponder_id);
 int decode_legacy(const uint8_t *softbits, uint32_t *transponder_id);
+int decode_rc4(const uint8_t *softbits, uint32_t *transponder_id);
 
 inline constexpr TransponderProps TRANSPONDER_PROPERTIES[] = {
     {0xf9a8, 80, "OPN"},
-    {0x51e4, 80, "AMB"}
+    {0x51e4, 80, "RC3"},
+    {0x6969, 16, "RC4"} 
+    //{0x7920, 80, "RC4"} //30
+    //{0x9617, 80, "RC4"}   //1
+    //{0x7916, 16, "RC4"}
 };
 
 constexpr TransponderProps transponder_props(TransponderType t) {
